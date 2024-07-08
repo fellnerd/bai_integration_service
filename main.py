80200362c58d1cd3b7595a44c18d879da4f9d73c
Daniel Fellner <admin@dimetrics.io>
2024-07-08 14:30:00 +0000

    host to lcalhost

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def create_app(config=None):
     # Flask Configuration
     app.config.from_mapping(
         DEBUG=os.environ.get('DEBUG', 'True').lower() in ['true', '1', 't'],
-        # MONGO_URI="mongodb://localhost:27017/",
-        MONGO_URI="mongodb://10.0.0.51:27017/",
+        MONGO_URI="mongodb://localhost:27017/",
         DATABASE_NAME='BAI_PROD_DB_KIES'
         # ... any other configuration
     )
